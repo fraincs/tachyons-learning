215a0e85e661be052bad5ca9f20498d2d0884ee6
Francis Thibault <francis@officevibe.com>
2018-10-30 12:34:56 -0400

    youtube preparation

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,12 @@ import './App.css';
 
 import React, { Component } from 'react';
 
+// importations des icones
+// import { ReactComponent as AudioSpeaker } from "./assets/audio-speaker.svg";
 class App extends Component {
   render() {
     return <div className="App">
         <div className="grid">
-            <div className="content">
-                <p>Content</p>
-            </div>
-            <div className="sidebar">
-                <p>Sidebar</p>
-            </div>
         </div>
       </div>;
   }
