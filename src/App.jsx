b1d2cf49182b5dd397eb8d7687d7502574883d20
Francis Thibault <francis@officevibe.com>
2018-10-31 13:27:33 -0400

    Roboto font added - exemples import svg et img

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import "./_globals/_colors.css";
 import './App.css';
 
 import React, { Component } from 'react';
+// importations d'icones
+// import { ReactComponent as LogoDark } from "./assets/on_platform_logo_dark.svg";
+
+// importations d'images
+// import avatar from "./assets/avatar.png";
 
 class App extends Component {
   render() {
     return <div className="App">
-        
       </div>;
   }
 }
